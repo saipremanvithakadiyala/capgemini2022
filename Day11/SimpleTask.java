70e11e9eefe74f9e55df88edaebbf89267a7983b
saipremanvithakadiyala <saipremanvitha.kadiyala.com>
2022-03-03 22:28:12 +0530

    my day1 java prgs

---
+++ Day11/SimpleTask.java
@@ -0,0 +1,24 @@
+package com.demo.collections;
+
+public class SimpleTask  implements Runnable{
+
+	private long sleepTime;
+
+	public SimpleTask(long sleepTime) {
+		super();
+		this.sleepTime = sleepTime;
+	}
+  //logic
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				System.out.println("Simple task is running on " + Thread.currentThread().getName() + " with priority " + Thread.currentThread().getPriority());
+				Thread.sleep(sleepTime);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
